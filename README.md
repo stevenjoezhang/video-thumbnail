77f196c3ba06b7efb3ebce4740bdc141498f31d6
StevenJoeZhang <1119186082@qq.com>
2018-06-10 00:06:23 +0800

    Bug fix

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
 # Video Thumbnail
-Create video thumbnails using shell, ffmpeg and python3.  
-基于shell, ffmpeg和python3创建视频在不同时间的截图，并将它们合成一张缩略图。
+Create video thumbnails using shell, ffmpeg and python (support python2 and python3).  
+基于shell, ffmpeg和python创建视频在不同时间的截图，并将它们合成一张缩略图。
 
 **Note**: Support \*nix systems only.
 ## Requirements
-Use `apt-get`, `yum` or `brew` to install ffmpeg and python3 first.
+ffmpeg and python are required. You can use `apt-get`, `yum` or `brew` to install them.
 
 ## Install
-```
+```bash
 git clone https://github.com/stevenjoezhang/video-thumbnail.git
 cd video-thumbnail
 ```
 
 ## Run
-```
+```bash
 sudo chmod +x ffmpeg.sh
-ffmpeg.sh Example.mp4
+bash ffmpeg.sh /path/to/example.mp4
 ```
 
 ## Usage
 Options:
-```
+```bash
 
 ```
 
--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,11 +1,9 @@
 #！/bin/bash
 
-#ffmpeg -i name.mp4 -y -f mjpeg -ss 3 -t 1 -s 320x240 output;
-#ffmpeg -i 坂本op.mp4 2>&1 | grep 'Duration' | cut -d ' ' -f 4 | sed s/,//
 time=$(ffmpeg -i $1 2>&1|grep 'Duration'|cut -d ' ' -f 4|sed s/,//);
-view=$(ffmpeg -i $1 2>&1|grep 'DAR'|cut -d '[' -f 2|cut -d ']' -f 1|cut -d 'R' -f 3|sed 's/^[ \t]*//g');
-w=$(echo $view|cut -d ':' -f 1);
-h=$(echo $view|cut -d ':' -f 2);
+view=$(ffmpeg -i $1 2>&1|grep 'Video:'|sed 's/.*\,.\(.*x[0-9]\{1,\}\).*/\1/g');
+w=$(echo $view|cut -d 'x' -f 1);
+h=$(echo $view|cut -d 'x' -f 2);
 height=$(echo 320*$h/$w|bc);
 #echo $height;
 hours=$(echo $time|cut -d ':' -f 1);
@@ -22,4 +20,4 @@ do
 temp=$(echo $duration*$i/50|bc);
 ffmpeg -ss $temp -i $1 -y -f mjpeg -t 0.001 -s 320x$height ./$dir/$i.jpg
 done
-python3 image.py $dir $height;
+python image.py $dir $height;
